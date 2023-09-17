06ee7e611be837dd99f83ac0855f721f4433b137
HeySleare <57966593+HeySleare@users.noreply.github.com>
2023-09-17 19:31:31 +0300

    burger

--- js/index.js
+++ js/index.js
@@ -2,4 +2,10 @@
 document.getElementById('check-box').onclick = function() {
     let selected = document.querySelector('input[type=radio][name=day]:checked');
     document.getElementById("galerry-image").src = `img/gallery/galerry_${selected.value}.png`;
+    document.getElementById("dot-in-dot").src = `img/icons/ellipse-5.png`;
 }
+$(document).ready(function(){
+    $('.header__burger').click(function(event){
+        $('.header__burger,.header-menu').toggleClass('active');
+    });
+});
