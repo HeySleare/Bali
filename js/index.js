907f04bb96512ec6b962227f3e6e905ec6bfaf04
HeySleare <57966593+HeySleare@users.noreply.github.com>
2023-09-20 17:31:07 +0300

    slider

--- js/index.js
+++ js/index.js
@@ -9,3 +9,27 @@ $(document).ready(function(){
         $('.header__burger,.header-menu').toggleClass('active');
     });
 });
+
+let offset = 0;
+const revievsContainer = document.querySelector('.revievs-container');
+document.querySelector('.left-button').addEventListener('click', function(){
+        offset += 555;
+        if(offset >= 1110){
+            offset=1110;
+            document.querySelector('.left-button').style.background = `#434347`;
+        }
+        document.querySelector('.right-button').style.background = `#fff`;
+        revievsContainer.style.left = -offset +`px`;
+        console.log(offset);
+});
+document.querySelector('.right-button').addEventListener('click', function(){
+    offset -= 555;
+    if(offset <= 0){
+        offset=0;
+        document.querySelector('.right-button').style.background = `#434347`;
+    }
+    document.querySelector('.left-button').style.background = `#fff`;
+    revievsContainer.style.left = -offset +`px`;
+    console.log(offset);
+});
+
